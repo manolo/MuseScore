1cd64d643b55a3961141192fe7269ac7bf182ea0
Manolo Carrasco <manolo@apache.org>
2026-06-15 17:02:35 +0200

    enc importer: show specific error message for encrypted ZBOT files
    Files with ZBOT/ZBOP/ZBO6 magic were created by very old versions of
    Encore (prior to 4.x) using a proprietary encryption. Instead of the
    generic "Bad format" error, show a message that explains the situation
    and tells the user to re-save the file using Encore 5.

--- src/importexport/encore/internal/notationencreader.cpp
+++ src/importexport/encore/internal/notationencreader.cpp
@@ -24,12 +24,32 @@
 #include "importer/import.h"
 
 #include "engraving/engravingerrors.h"
+#include "translation.h"
+
+#include <QFile>
 
 using namespace mu::iex::encore;
 using namespace mu::engraving;
 
 muse::Ret NotationEncoreReader::read(MasterScore* score, const muse::io::path_t& path, const Options&)
 {
+    // ZBOT / ZBOP / ZBO6 are encrypted formats produced by very old versions
+    // of Encore (prior to 4.x). Decryption is not supported; show a specific
+    // message so the user knows what to do instead of a generic "Bad format".
+    {
+        QFile f(path.toQString());
+        if (f.open(QIODevice::ReadOnly)) {
+            const QByteArray magic = f.read(4);
+            if (magic.startsWith("ZBO")) {
+                return make_ret(Err::FileBadFormat,
+                                muse::mtrc("iex_encore",
+                                           "This file was created with a very old version of Encore (prior to 4.x) "
+                                           "and uses an encrypted format that is not supported.\n"
+                                           "Please open it in Encore 4.x or 5.x and re-save it as a regular .enc file."));
+            }
+        }
+    }
+
     Err err = importEncore(score, path.toQString());
     return make_ret(err, path);
 }
